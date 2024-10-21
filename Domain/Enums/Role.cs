e7e557a1a3346b6e5f9fb8318cfdf98e07af8db2
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2024-10-21 19:25:24 +0300

    Seeded SuperAdministrator

---
+++ Domain/Enums/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Enums
+{
+	public enum Role
+	{
+		SuperAdministrator,
+		Administrator,
+		Manager,
+		Instructor
+	}
+}
--- Infrastructure/Settings/AccountSettings.cs
+++ Infrastructure/Settings/AccountSettings.cs
@@ -2,5 +2,10 @@
 
 public class AccountSettings
 {
-
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Username { get; set; } = null!;
+    public string? Role { get; set; } = null!;
 }
