78d796d1d142f06af25c48b8ce910d4e1715f488
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2025-06-21 11:09:24 +0300

    Implementing TemplateRenderer

---
+++ Application/Common/Services/TemplateRenderer.cs
@@ -0,0 +1,25 @@
+﻿using Application.Common.Models;
+using System.Text;
+
+namespace Application.Common.Services
+{
+    public class TemplateRenderer : ITemplateRenderer
+    {
+        public string Fill(Template template, IEnumerable<TemplateParameterModel> parameters)
+        {
+            string content = template.Content;
+
+            StringBuilder builder = new StringBuilder(content);
+
+            foreach (TemplateParameterModel parameter in parameters)
+            {
+                string name = parameter.Name;
+                string value = parameter.Value;
+
+                builder.Replace($"{{{{{name}}}}}", value);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
