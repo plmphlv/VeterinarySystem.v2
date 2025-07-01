e4cb85635378111f6920ab8705fd805254204c94
Plamen Pehlivanov <105740303+plmphlv@users.noreply.github.com>
2025-07-01 20:05:10 +0300

    Adding TemplatesController to manage email template operations.

---
+++ Application/Templates/Queries/GetTemplates/TemplateDto.cs
@@ -0,0 +1,17 @@
+﻿using Application.Templates.Common;
+
+namespace Application.Templates.Queries.GetTemplates
+{
+    public class TemplateDto : TemplateModel
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+
+        public string CreatedBy { get; set; } = null!;
+
+        public string? LastModifiedBy { get; set; }
+    }
+}
--- Web.API/Controllers/TemplatesController.cs
+++ Web.API/Controllers/TemplatesController.cs
@@ -1,7 +1,51 @@
-﻿namespace Web.API.Controllers
+﻿using Application.Templates.Commands.Create;
+using Application.Templates.Commands.Delete;
+using Application.Templates.Commands.Update;
+using Application.Templates.Queries.GetTemplateDetails;
+using Application.Templates.Queries.GetTemplates;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.API.Controllers
 {
     public class TemplatesController : ApiControllerBase
     {
+        [HttpPost]
+        public async Task<ActionResult<int>> Create([FromBody] CreateTemplateCommand command)
+        {
+            return await Mediator.Send(command);
+        }
 
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete([FromRoute] int id)
+        {
+            await Mediator.Send(new DeleteTemplateCommand { Id = id });
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Update([FromBody] int id, [FromBody] UpdateTemplateCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            await Mediator.Send(command);
+
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TemplateOutputModel>> GetTemplateDetails([FromRoute] int id)
+        {
+            return await Mediator.Send(new GetTemplateDetailsQuery { Id = id });
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TemplateDto>>> GetTemplates([FromQuery] GetTemplatesQuery query)
+        {
+            return await Mediator.Send(query);
+        }
     }
 }
