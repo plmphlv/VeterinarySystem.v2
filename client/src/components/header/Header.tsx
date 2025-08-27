9b92d87afd427d7cf1e2925bf25568d54b942907
b0zh1d4r <bozhidarivanov24@gmail.com>
2025-08-27 14:40:24 +0300

    Some cool Header style changes

--- client/src/components/header/Header.tsx
+++ client/src/components/header/Header.tsx
@@ -1,37 +1,65 @@
-import { Link } from "react-router";
+import { NavLink, useLocation } from "react-router";
 import { useUserContext } from "../../contexts/UserContext";
+import { useEffect, useRef } from "react";
 
 const Header: React.FC = () => {
     const { isSuccessful } = useUserContext();
+    const location = useLocation();
+    const checkRef = useRef<HTMLInputElement>(null);
+    const headerRef = useRef<HTMLElement>(null);
+
+    // Затваря менюто, когато се кликне върху линк
+    useEffect(() => {
+        if (checkRef.current) {
+            checkRef.current.checked = false;
+        }
+    }, [location.pathname]);
+
+    // Затваря менюто при клик извън хедъра
+    useEffect(() => {
+        const handleClickOutside = (e: MouseEvent) => {
+            if (
+                checkRef.current &&
+                headerRef.current &&
+                !headerRef.current.contains(e.target as Node) &&
+                checkRef.current.checked
+            ) {
+                checkRef.current.checked = false;
+            }
+        };
+
+        document.addEventListener("click", handleClickOutside);
+        return () => document.removeEventListener("click", handleClickOutside);
+    }, []);
 
     return (
-        <header className="header">
-            <Link to="/" className="logo">
+        <header className="header" ref={headerRef}>
+            <NavLink to="/" className="logo">
                 <img src="/images/veteriq.png" alt="Vetariq" width="60px" />
-            </Link>
+            </NavLink>
 
-            <input type="checkbox" id="check" />
+            <input type="checkbox" id="check" ref={checkRef} />
             <label htmlFor="check" className="icons">
                 <i className="bx bx-menu" id="menu-icon"></i>
                 <i className="bx bx-x" id="close-icon"></i>
             </label>
 
             <nav className="navbar">
-                <Link to="/" style={{ "--i": "0" } as React.CSSProperties}>Home</Link>
-                <Link to="/about" style={{ "--i": "1" } as React.CSSProperties}>About</Link>
-                <Link to="/contact" style={{ "--i": "2" } as React.CSSProperties}>Contact</Link>
-                <Link to="/services" style={{ "--i": "3" } as React.CSSProperties}>Services</Link>
+                <NavLink to="/" style={{ "--i": "0" } as React.CSSProperties}>Home</NavLink>
+                <NavLink to="/about" style={{ "--i": "1" } as React.CSSProperties}>About</NavLink>
+                <NavLink to="/contact" style={{ "--i": "2" } as React.CSSProperties}>Contact</NavLink>
+                <NavLink to="/services" style={{ "--i": "3" } as React.CSSProperties}>Services</NavLink>
 
                 {isSuccessful ? (
                     <>
-                        <Link to="/my-pets" style={{ "--i": "4" } as React.CSSProperties}>My Pets</Link>
-                        <Link to="/profile" style={{ "--i": "5" } as React.CSSProperties}>Profile</Link>
-                        <Link to="/logout" style={{ "--i": "6" } as React.CSSProperties}>Logout</Link>
+                        <NavLink to="/my-pets" style={{ "--i": "4" } as React.CSSProperties}>My Pets</NavLink>
+                        <NavLink to="/profile" style={{ "--i": "5" } as React.CSSProperties}>Profile</NavLink>
+                        <NavLink to="/logout" style={{ "--i": "6" } as React.CSSProperties}>Logout</NavLink>
                     </>
                 ) : (
                     <>
-                        <Link to="/login" style={{ "--i": "4" } as React.CSSProperties}>Login</Link>
-                        <Link to="/register" style={{ "--i": "5" } as React.CSSProperties}>Register</Link>
+                        <NavLink to="/login" style={{ "--i": "4" } as React.CSSProperties}>Login</NavLink>
+                        <NavLink to="/register" style={{ "--i": "5" } as React.CSSProperties}>Register</NavLink>
                     </>
                 )}
             </nav>
